2cf7487992d71adfde10a9cab325b53f95257cf2
Matthew <casagrande.theus@gmail.com>
2026-02-15 01:18:27 -0300

    Feat: Service de ComposicaoTimeService comentado para buildar o projeto e validar o H2.

---
+++ src/main/java/br/com/duxusdesafio/service/ComposicaoTimeService.java
@@ -0,0 +1,19 @@
+package br.com.duxusdesafio.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ComposicaoTimeService {
+
+  //Atributos de Composição de Time
+  //private final ComposicaoTimeRepository composicaoTimeRepository;
+
+//  public ComposicaoTimeService (ComposicaoTimeRepository composicaoTimeRepository){
+//    this.composicaoTimeRepository = composicaoTimeRepository;
+//  }
+
+  public String contagemEstatisticaPorFuncao(){
+    // TODO Implementar método seguindo as instruções!
+    return null;
+  }
+}
