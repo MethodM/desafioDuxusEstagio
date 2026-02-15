6b008136ae943367f6f17583721e7bbc813de843
Matthew <casagrande.theus@gmail.com>
2026-02-15 01:16:30 -0300

    Feat: Controller TimeController comentado para build do projeto.

---
+++ src/main/java/br/com/duxusdesafio/controller/TimeController.java
@@ -0,0 +1,32 @@
+package br.com.duxusdesafio.controller;
+
+import br.com.duxusdesafio.controller.request.CadastroTimeRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/times")
+public class TimeController {
+
+  //@Autowired
+  //private final TimeService timeService;
+  //TODO: Implementar os endpoints para criar, listar, atualizar e deletar times
+
+  @PostMapping("/cadastroTime")
+  public ResponseEntity<?> cadastroDeTime(@RequestBody CadastroTimeRequest time) {
+//    return ResponseEntity.ok(timeService.criar(time));
+    return ResponseEntity.ok("test.criar(time)");
+  }
+
+  @DeleteMapping("/removerTime/{id}")
+  public ResponseEntity<?> removerTime(@PathVariable Long id) {
+//    timeService.deletarTime(id);
+    return ResponseEntity.ok("Time removido com sucesso");
+  }
+
+}
