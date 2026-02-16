5148d970ded9b61d3f90ad626b694ce5f9a53fc8
Matthew <casagrande.theus@gmail.com>
2026-02-16 12:45:01 -0300

    Feat: Alteração em ComposicaoTimeService para salvar composição do time e seus integrantes.

--- src/main/java/br/com/duxusdesafio/service/ComposicaoTimeService.java
+++ src/main/java/br/com/duxusdesafio/service/ComposicaoTimeService.java
@@ -1,19 +1,23 @@
 package br.com.duxusdesafio.service;
 
+import br.com.duxusdesafio.model.ComposicaoTime;
+import br.com.duxusdesafio.model.Integrante;
+import br.com.duxusdesafio.model.Time;
+import br.com.duxusdesafio.repository.ComposicaoTimeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ComposicaoTimeService {
 
-  //Atributos de Composição de Time
-  //private final ComposicaoTimeRepository composicaoTimeRepository;
+  @Autowired
+  private final ComposicaoTimeRepository composicaoTimeRepository;
 
-//  public ComposicaoTimeService (ComposicaoTimeRepository composicaoTimeRepository){
-//    this.composicaoTimeRepository = composicaoTimeRepository;
-//  }
+  public ComposicaoTimeService (ComposicaoTimeRepository composicaoTimeRepository){
+    this.composicaoTimeRepository = composicaoTimeRepository;
+  }
 
-  public String contagemEstatisticaPorFuncao(){
-    // TODO Implementar método seguindo as instruções!
-    return null;
+  public ComposicaoTime create(Time time, Integrante integrante){
+    return composicaoTimeRepository.save(new ComposicaoTime(time, integrante));
   }
 }
