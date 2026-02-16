8ae3488d1bd85ed997275367c0332d873058c323
Matthew <casagrande.theus@gmail.com>
2026-02-16 12:47:57 -0300

    Feat: Aprimoramento na classe IntegranteService com a implementação do método 'cadastraOuRetorna' para trabalhar com setter de atributos e salvar informações no repository.

--- src/main/java/br/com/duxusdesafio/service/IntegranteService.java
+++ src/main/java/br/com/duxusdesafio/service/IntegranteService.java
@@ -1,5 +1,6 @@
 package br.com.duxusdesafio.service;
 
+import br.com.duxusdesafio.model.Integrante;
 import br.com.duxusdesafio.repository.IntegranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,4 +15,16 @@ public class IntegranteService {
     this.integranteRepository = integranteRepository;
   }
 
+  public Integrante cadastraOuRetorna(String nome, String funcao, String franquia) {
+    if(integranteRepository.existsByNomeAndFuncaoAndFranquia(nome, funcao, franquia)){
+      return integranteRepository.findByNomeAndFuncaoAndFranquia(nome, funcao, franquia);
+    } else {
+      Integrante integrante = new Integrante();
+      integrante.setNome(nome);
+      integrante.setFuncao(funcao);
+      integrante.setFranquia(franquia);
+      return integranteRepository.save(integrante);
+    }
+
+  }
 }
