bcce712db0b3733d71f2ee019604e9c59c961fda
Matthew <casagrande.theus@gmail.com>
2026-02-16 17:14:16 -0300

    Feat: Criação de GetMapping - '/integranteMaisUsado' para realização de teste via Postman com JSON.

--- src/main/java/br/com/duxusdesafio/controller/ApiController.java
+++ src/main/java/br/com/duxusdesafio/controller/ApiController.java
@@ -1,23 +1,21 @@
 package br.com.duxusdesafio.controller;
 
-import br.com.duxusdesafio.controller.request.CadastroTimeRequest;
 import br.com.duxusdesafio.controller.response.IntegrantesResponse;
 import br.com.duxusdesafio.controller.response.TimeDaDataResponse;
 import br.com.duxusdesafio.model.ComposicaoTime;
+import br.com.duxusdesafio.model.Integrante;
 import br.com.duxusdesafio.model.Time;
 import br.com.duxusdesafio.repository.TimeRepository;
 import br.com.duxusdesafio.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,6 +43,7 @@ public class ApiController {
 
     List<IntegrantesResponse> integrantesResponseList = new ArrayList<>();
 
+    // para cada composição do time, criar um IntegrantesResponse e adicionar à lista
     for (ComposicaoTime composicaoTime : time.getComposicaoTime()) {
       IntegrantesResponse integrantesResponse = new IntegrantesResponse();
       integrantesResponse.setNome(composicaoTime.getIntegrante().getNome());
@@ -56,5 +55,27 @@ public class ApiController {
     timeDaDataResponse.setIntegrantes(integrantesResponseList);
 
     return ResponseEntity.ok(timeDaDataResponse);
+  }
+
+  @GetMapping("/integranteMaisUsado")
+  public ResponseEntity<?> getIntegranteMaisUsado(
+      @RequestParam String dataInicial,
+      @RequestParam String dataFinal) {
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    LocalDate inicio = LocalDate.parse(dataInicial, formatter);
+    LocalDate fim = LocalDate.parse(dataFinal, formatter);
+
+    List<Time> todosOsTimes = timeRepository.findAll();
+
+    Integrante integrante = apiService.integranteMaisUsado(inicio, fim, todosOsTimes);
+
+    IntegrantesResponse response = new IntegrantesResponse();
+    response.setNome(integrante.getNome());
+    response.setFuncao(integrante.getFuncao());
+    response.setFranquia(integrante.getFranquia());
+
+    return ResponseEntity.ok(response);
   }
 }
