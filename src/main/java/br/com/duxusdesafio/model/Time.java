d5c878f983f4fc22a83975079def1e2d18f4c2af
Matthew <casagrande.theus@gmail.com>
2026-02-15 21:45:51 -0300

    Feat: Adição de atributo "nomeTime" à Classe 'Time', para testes JSON.

--- src/main/java/br/com/duxusdesafio/model/Time.java
+++ src/main/java/br/com/duxusdesafio/model/Time.java
@@ -9,67 +9,77 @@ import java.util.Objects;
 @Table(name = "time")
 public class Time {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
 
-	@Column
-    private LocalDate data;
-	
-	@OneToMany(mappedBy = "time", cascade = CascadeType.ALL)
-	private List<ComposicaoTime> composicaoTime;
+  @Column
+  private String nomeTime;
 
-	public Time() {
-	}
+  @Column
+  private LocalDate data;
 
-	public Time(LocalDate data, List<ComposicaoTime> composicaoTime) {
-		this.data = data;
-		this.composicaoTime = composicaoTime;
-	}
+  @OneToMany(mappedBy = "time", cascade = CascadeType.ALL)
+  private List<ComposicaoTime> composicaoTime;
 
-	public long getId() {
-		return id;
-	}
+  public Time() {
+  }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+  public Time(LocalDate data, List<ComposicaoTime> composicaoTime) {
+    this.data = data;
+    this.composicaoTime = composicaoTime;
+  }
 
-	public LocalDate getData() {
-		return data;
-	}
+  public long getId() {
+    return id;
+  }
 
-	public void setData(LocalDate data) {
-		this.data = data;
-	}
+  public void setId(long id) {
+    this.id = id;
+  }
 
-	public List<ComposicaoTime> getComposicaoTime() {
-		return composicaoTime;
-	}
+  public String getNomeTime() {
+    return nomeTime;
+  }
+  public void setNomeTime(String nomeTime) {
+    this.nomeTime = nomeTime;
+  }
 
-	public void setComposicaoTime(List<ComposicaoTime> composicaoTime) {
-		this.composicaoTime = composicaoTime;
-	}
+  public LocalDate getData() {
+    return data;
+  }
+
+  public void setData(LocalDate data) {
+    this.data = data;
+  }
+
+  public List<ComposicaoTime> getComposicaoTime() {
+    return composicaoTime;
+  }
+
+  public void setComposicaoTime(List<ComposicaoTime> composicaoTime) {
+    this.composicaoTime = composicaoTime;
+  }
 
 
-	@Override
-	public final boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof Time)) return false;
-		Time time = (Time) o;
-		return id == time.id && Objects.equals(data, time.data);
-	}
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Time)) return false;
+    Time time = (Time) o;
+    return id == time.id && Objects.equals(data, time.data);
+  }
 
-	@Override
-	public final int hashCode() {
-		return Objects.hash(id, data);
-	}
+  @Override
+  public final int hashCode() {
+    return Objects.hash(id, data);
+  }
 
-	@Override
-	public String toString() {
-		return "Time{" +
-				"id=" + id +
-				", data=" + data +
-				'}';
-	}
+  @Override
+  public String toString() {
+    return "Time{" +
+        "id=" + id +
+        ", data=" + data +
+        '}';
+  }
 }
