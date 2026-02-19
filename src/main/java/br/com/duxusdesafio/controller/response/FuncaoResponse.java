d895a18edf2922e086030dfaa82c8e88a948272f
Matthew <casagrande.theus@gmail.com>
2026-02-18 23:17:38 -0300

    Feat: add FuncaoResponse

---
+++ src/main/java/br/com/duxusdesafio/controller/response/FuncaoResponse.java
@@ -0,0 +1,13 @@
+package br.com.duxusdesafio.controller.response;
+
+public class FuncaoResponse {
+  private String funcao;
+
+  public String getFuncao() {
+    return funcao;
+  }
+
+  public void setFuncao(String funcao) {
+    this.funcao = funcao;
+  }
+}
